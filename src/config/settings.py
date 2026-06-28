2e8281fbc11824f2c593031ac06598970db090bd
Gabriel Ratão <gabiratao28@gmail.com>
2026-06-28 16:21:43 -0300

    sync new features to main

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,7 +1,11 @@
 # src/config/settings.py
+
 import yaml
 
-def carregar_config(path: str = "./config/settings.yaml") -> dict:
-    """Carrega um arquivo de configuração YAML."""
-    with open(path, "r") as file:
-        return yaml.safe_load(file)
+
+class Settings:
+
+    def carregar_config(self, path: str = "./config/settings.yaml") -> dict:
+        """Carrega um arquivo de configuração YAML."""
+        with open(path, "r", encoding="utf-8") as file:
+            return yaml.safe_load(file)
